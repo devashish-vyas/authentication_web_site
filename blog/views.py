8d907b9babead26f4f2a4692c9427ea7e20f076c
devashish vyas <devashishvyas77@gmail.com>
2021-05-31 16:33:25 +0530

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.urls.base import reverse
 from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 # Create your views here.
 
@@ -33,3 +36,55 @@ context2={
 
 def about(request):
     return render(request,'blog/about.html',context2)
+
+
+class PostListView(ListView):
+    model=Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']#for post order newest to oldest
+
+#<app>/<model>_<viewtype>.html
+
+
+class PostDetailView(DetailView):
+    model=Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView): # return redirect will not work as it is a class based vuiew not a function based view
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+
+
+class PostUpdateView(UserPassesTestMixin,LoginRequiredMixin,UpdateView): # return redirect will not work as it is a class based vuiew not a function based view
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+
+
+class PostDeleteView(UserPassesTestMixin,LoginRequiredMixin,DeleteView):
+    model=Post
+    success_url='/'
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+
+
+   
